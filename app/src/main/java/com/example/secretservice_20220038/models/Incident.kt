1a4979efbf914b50bb351517f08e68d6b04a50ad
eeceballos <emmanuel.ec2904@gmail.com>
2024-07-19 21:07:03 -0400

    fix: add name to manifest

--- app/src/main/java/com/example/secretservice_20220038/models/Incident.kt
+++ app/src/main/java/com/example/secretservice_20220038/models/Incident.kt
@@ -1,8 +1,12 @@
 package com.example.secretservice_20220038.models
 
 import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
 
 class Incident: RealmObject {
+    @PrimaryKey
+    var id: ObjectId = ObjectId()
     var title: String = ""
     var date: String = ""
     var description: String = ""
